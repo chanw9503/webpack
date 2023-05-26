9a4d5b416a9d8653edf18ec9dbc324432463fd32
ChanwooPark <chanw9503@gmail.com>
2023-05-26 23:33:41 +0900

    🔧  플러그인 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path'); //node.js에서 파일의 경로를 쉽게 핸들링 할 수 있게 도와주는 부품
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './source/index.js', // 입구에 해당하는 파일 입력
+  entry: {
+    index: './source/index.js',
+    about: './source/about.js',
+  },
   output: {
     path: path.resolve(__dirname, 'public'), //__dirname : 현재 webpack.config.js 파일이 위치하고 있는 경로를 알려주는 변수  // public 이라는 하위 경로에 최종적인 결과물을 갖다 놓으라는 뜻
-    filename: 'index_bundle.js', //생성될 파일의 이름
+    filename: '[name]_bundle.js', //생성될 파일의 이름
   },
   module: {
     rules: [
@@ -16,4 +20,16 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './source/index.html',
+      filename: './index.html',
+      chunks: ['index'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './source/about.html',
+      filename: './about.html',
+      chunks: ['about'],
+    }),
+  ],
 };
